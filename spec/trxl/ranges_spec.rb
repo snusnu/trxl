8319ee41dd5e736e395c4cf0146f8c8203e01e03
snusnu <gamsnjaga@gmail.com>
2011-04-27 11:05:28 +0200

    Move string specs to spec/trxl/strings_spec.rb

--- spec/trxl/ranges_spec.rb
+++ spec/trxl/ranges_spec.rb
@@ -59,23 +59,3 @@ describe "When working with Ranges, the Trxl::Calculator" do
   end
 
 end
-
-describe "When working with Strings, the Trxl::Calculator" do
-
-  include Trxl::SpecHelper
-  
-  before(:each) do
-    @parser = Trxl::Calculator.new
-  end
-  
-  it "should evaluate string literals to strings using double quotes (\"\")" do
-    eval("\"Test String\"").should == "Test String"
-    eval("s = \"Test String\"; s;").should == "Test String"
-  end
-  
-  it "should evaluate string literals to strings using single quotes ('')" do
-    eval("'Test String'").should == "Test String"
-    eval("s = 'Test String'; s;").should == "Test String"
-  end
-  
-end
--- spec/trxl/strings_spec.rb
+++ spec/trxl/strings_spec.rb
@@ -1,1 +1,21 @@
 require 'spec_helper'
+
+describe "When working with Strings, the Trxl::Calculator" do
+
+  include Trxl::SpecHelper
+
+  before(:each) do
+    @parser = Trxl::Calculator.new
+  end
+
+  it "should evaluate string literals to strings using double quotes (\"\")" do
+    eval("\"Test String\"").should == "Test String"
+    eval("s = \"Test String\"; s;").should == "Test String"
+  end
+
+  it "should evaluate string literals to strings using single quotes ('')" do
+    eval("'Test String'").should == "Test String"
+    eval("s = 'Test String'; s;").should == "Test String"
+  end
+
+end
